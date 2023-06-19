504089af87666b3426b370edfb9512c2cd30a06e
mholi <mholipresi@gmail.com>
2023-06-19 07:14:17 -0700

    9-print_comb.c

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -10,12 +10,9 @@ int main(void)
 
 	for (a = 0; a < 10; a++;)
 	{
-		putchar(a + '0');
-		if (a < 0)
-		{
-			putchar(',');
-			putchar(' ');
-		}
+		printf("%d", a);
+		if (a < 9)
+			printf(", ");
 	}
 	putchar('\n');
 	return (0);
