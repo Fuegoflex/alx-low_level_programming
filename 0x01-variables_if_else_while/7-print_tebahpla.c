0fb4cfd10092e9405ffc7d87b289cbcc4f613576
mholi <mholipresi@gmail.com>
2023-06-19 06:37:35 -0700

    7-print_tebahpla.c

---
+++ 0x01-variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return:0 (Success)
+ */
+
+int main(void)
+{
+	chat x;
+
+	x = 'z';
+	while (x >= 'a')
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
